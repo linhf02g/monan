d6754b335c471c9f2b258d7205ae4e1b11030a3a
linhf02g <linhf02g@gmail.com>
2018-01-15 15:45:51 +0100

    Add files via upload

---
+++ test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+//program test(
+int random();
+int random()
+{
+	srand(time(NULL)),
+	int r;
+	r = rand()%45+1,
+}
+int prmonan();
+int prmonan()
+{
+	int m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15,m16,m17,m18,m19,m20,m41,m42,m43;
+	int m21,m22,m23,m24,m25,m26,m27,m28,m29,m30,m31,m32,m33,m34,m35,m36,m37,m38,m39,m40;
+	printf("Test ten mon an va so\n");
+	if (random()=1)
+	{
+		printf("Sup Rau:\n", );
+		scanf("%d",&m1)
+		if (m1=1)
+			printf("dung\n");
+		return main();
+	}
+	if(random()=2)
+	{
+		printf("Sup Ga\n", );
+	}
+
+
+}
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
